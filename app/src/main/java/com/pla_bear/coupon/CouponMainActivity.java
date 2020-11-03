70acbd5ebe06a3f44b3e26e3bb4f8eaf27d24f72
dmbs335 <53107595+dmbs335@users.noreply.github.com>
2020-11-03 17:54:43 +0900

    CouponMainActivity update

--- app/src/main/java/com/pla_bear/coupon/CouponMainActivity.java
+++ app/src/main/java/com/pla_bear/coupon/CouponMainActivity.java
@@ -1,7 +1,19 @@
 package com.pla_bear.coupon;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pla_bear.R;
 
 public class CouponMainActivity extends AppCompatActivity {
@@ -10,5 +22,15 @@ public class CouponMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupon_main);
+        Button button = findViewById(R.id.btn_coupon_register);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.edit_coupon_num);
+                Editable value = editText.getText();
+                Log.d("디버그", value.toString());
+            }
+        });
     }
 }
