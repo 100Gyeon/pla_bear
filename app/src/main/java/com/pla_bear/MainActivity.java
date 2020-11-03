984a20747ebea9a05f9f10614003362740e7678d
100Gyeon <bjy9966@naver.com>
2020-11-03 17:36:36 +0900

    Make basic frame for app

--- app/src/main/java/com/pla_bear/MainActivity.java
+++ app/src/main/java/com/pla_bear/MainActivity.java
@@ -1,14 +1,80 @@
 package com.pla_bear;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.pla_bear.coupon.CouponMainActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawer;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawer = findViewById(R.id.main_drawer);
+        toggle = new ActionBarDrawerToggle(this, drawer, R.string.drawer_open, R.string.drawer_close);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toggle.syncState();
+
+        NavigationView navigationView=findViewById(R.id.main_drawer_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.menu_drawer_1) {
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                } else if (id == R.id.menu_drawer_2) {
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(MainActivity.this, CouponMainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                } else if (id == R.id.menu_drawer_3) {
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(MainActivity.this, ChallengeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                } else if (id == R.id.menu_drawer_4) {
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                } else if (id == R.id.menu_drawer_5) {
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(MainActivity.this, BoardActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                } else if (id == R.id.menu_drawer_6) {
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return false;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
