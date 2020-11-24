3d00043fd71f95fa27ef821d68b87fa2ea33ae05
dmbs335 <53107595+dmbs335@users.noreply.github.com>
2020-11-25 01:03:14 +0900

    Minor fix

--- app/src/main/java/com/pla_bear/map/qrcode/QRCodeActivity.java
+++ app/src/main/java/com/pla_bear/map/qrcode/QRCodeActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.pla_bear.R;
 
 public class QRCodeActivity extends AppCompatActivity {
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        qrScan = new IntentIntegrator(this);
+        qrScan.setOrientationLocked(false);
+        qrScan.setPrompt("Sample Text");
+        qrScan.initiateScan();
     }
 }
